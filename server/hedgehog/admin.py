00cfb557b2cdc8d7adfb9f74d920f3153eab5b83
Kenny Song <mila_411@me.com>
2019-07-20 15:18:42 +0900

    clean up

--- server/hedgehog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(PileColor)
-admin.site.register(HedgeHog)
-admin.site.register(Comment)
--- server/hedgehog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HedgehogConfig(AppConfig):
-    name = 'hedgehog'
