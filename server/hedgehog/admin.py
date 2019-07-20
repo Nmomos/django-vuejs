7bb6e813aa7734c0ba57ee027344b7284133818a
Kenny Song <mila_411@me.com>
2019-07-20 15:16:56 +0900

    update ignore

--- server/hedgehog/admin.py
+++ server/hedgehog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(PileColor)
+admin.site.register(HedgeHog)
+admin.site.register(Comment)
